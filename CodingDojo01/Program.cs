c9676420aec720051dd1b8a26f3d3412ac685bc9
Ian <ianburrows.sa@gmail.com>
2017-09-15 22:19:05 +0200

    added celciusconverter

--- CodingDojo01/Program.cs
+++ CodingDojo01/Program.cs
@@ -15,6 +15,40 @@ namespace CodingDojo01
                 "2. Fahrenheit \n" +
                 "3. Reaumur \n" +
                 "4. Kelvin");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Enter a temperature to convert: ");
+                    string temp = Console.ReadLine();
+                    CelciusConverter(temp);
+                    Console.ReadLine();
+                    break;
+                //case "2":
+                //    fahrenheitconverter();
+                //    break;
+                //case "3":
+                //    reaumurconverter();
+                //    break;
+                //case "4":
+                //    kelvinconverter();
+                //    break;
+                default:
+                    Console.WriteLine("Please enter a number between 1 and 4.");
+                    break;
+            }
+
+        }
+
+        public static void CelciusConverter(string temp)
+        {
+            float newTemp = float.Parse(temp);
+            Console.WriteLine(temp + " Celcius converted: \n" +
+                "Fahrenheit: {0} \n" + 
+                "Reaumur: {1} \n" +
+                "Kelvin: {2}", (newTemp * 1.8 + 32), (newTemp * 0.8), (newTemp + 237.15));
         }
     }
 }
